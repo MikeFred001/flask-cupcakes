4045c2ae418d6d716f40fd1fc2c0fac8ea2b7939
jesse.chung9727@gmail.com <jesse.chung9727@gmail.com>
2023-07-31 17:29:42 -0400

    first code review

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DATABASE_URL", "postgresql:///adopt")
+    "DATABASE_URL", "postgresql:///cupcakes")
 
 toolbar = DebugToolbarExtension(app)
 
@@ -29,3 +29,40 @@ def list_all_cupcakes():
     serialized = [ cupcake.serialize() for cupcake in cupcakes ]
 
     return jsonify(cupcakes=serialized)
+
+
+@app.get('/api/cupcakes/<int:cupcake_id>')
+def list_single_cupcake(cupcake_id):
+    """Show information on one single cupcake.
+    Returns JSON {"cupcake": {id, flavor, size....}}"""
+
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+
+@app.post('/api/cupcakes')
+def create_cupcake():
+    """Make a new cupcake
+     Returns JSON {"cupcake": {id, flavor, size....}}
+    """
+
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image_url = request.json["image_url"]
+
+    new_cupcake = Cupcake(flavor=flavor,
+                          size=size,
+                          rating=rating,
+                          image_url=image_url)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
